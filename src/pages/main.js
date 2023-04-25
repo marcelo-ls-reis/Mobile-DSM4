111579e27f797fca7af28d9af2a85f82426a3747
marcelo-ls-reis <marcelo.reis5@fatec.sp.gov.br>
2023-04-25 20:20:17 -0300

    Aula 25/04/2023

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,76 +1,109 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container, Form, Input, SubmitButton, List, User, Avatar, Name, Bio, ProfileButton, ProfileButtonText } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 import api from '../services/api';
-import { Keyboard } from "react-native";
+import {Keyboard, ActivityIndicator} from 'react-native';
 
-export default class Main extends Component{
+export default class Main extends Component { 
+  state = {
+    // state is an object
+    newUser: '',
+    users: [],
+    loading: false,
+  };
 
-    state = { // state is an object
-        newUser: '',
-        users: [],
-    };   
-    
-    handleAddUser = async () => { 
-        const { users, newUser } = this.state;
+    async componentDidMount() { // busca os dados do AsyncStorage
+    const users = await AsyncStorage.getItem('users'); // get users para AsyncStorage
 
-        const response = await api.get(`/users/${newUser}`); 
-
-        const data = { // data is an object
-            name: response.data.name,
-            login: response.data.login,
-            bio: response.data.bio,
-            avatar: response.data.avatar_url,
-        };
-        console.log(data); 
-        
-        this.setState({ // setState is a function
-            users: [...users, data],
-            newUser: '',
-        });
-
-        Keyboard.dismiss();
-
+    if (users) {
+      this.setState({users: JSON.parse(users)}); // set users para state
     }
+}
 
-    render(){
+    async componentDidUpdate(_, prevState) { // salva os dados no AsyncStorage
+    const {users} = this.state; // desestruturando
 
-         const { users, newUser } = this.state;
+    if (prevState.users !== users) {
+       await AsyncStorage.setItem('users', JSON.stringify(users)); // set users para AsyncStorage
+    }
+}
+
+  handleAddUser = async () => { // adiciona um novo usuário
+    const {users, newUser} = this.state; // desestruturando
+
+    this.setState({loading: true}); // setando o loading para true
+
+    const response = await api.get(`/users/${newUser}`); // chamando a api
+
+    const data = {
+      // data is an object
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+    console.log(data);
+
+    this.setState({
+      // setState is a function
+      users: [...users, data],
+      newUser: '',
+      loading: false,
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const {users, newUser, loading} = this.state; // desestruturando
 
     return (
-        <Container>
-            <Form>
-                <Input
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    placeholder="Adicionar usuário"
-                    value={newUser}
-                    onChangeText={text => this.setState({ newUser: text })}
-                    returnKeyType="send"
-                    onSubmitEditing={this.handleAddUser}
-                />
-                <SubmitButton onPress={ this.handleAddUser }>
-                    <Icon name='add' size={20} color='#fff' />
-                </SubmitButton>
-            </Form>
-                <List
-                    showsVerticalScrollIndicator={false}
-                    data={users}
-                    keyExtractor={user => user.login}
-                    renderItem={({ item }) => (
-                        <User>
-                            <Avatar source={{ uri: item.avatar }} />
-                            <Name>{item.name}</Name>
-                            <Bio>{item.bio}</Bio>
-                        
-                        <ProfileButton onPress={() => {}}>
-                            <ProfileButtonText>Ver perfil</ProfileButtonText>
-                        </ProfileButton>
-                        </User>
-                    )}
-                />
-            
-        </Container>
-    )
-    }
+      <Container> 
+        <Form>
+          <Input
+            autoCorrect={false} 
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (<ActivityIndicator color="#fff" />) : (
+            <Icon name="add" size={20} color="#fff" />)}
+          </SubmitButton>
+        </Form>
+        <List
+          showsVerticalScrollIndicator={false}
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({item}) => (
+            <User>
+              <Avatar source={{uri: item.avatar}} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => {}}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
+      </Container>
+    );
+  }
 }
